ff7531da968378a39b41e26fa55579129126ab77
hedvi2005 <hedvi@nadenane.net>
2025-03-17 15:28:31 +0100

    Suppresion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -35,9 +35,7 @@
             <!-- Place Actuelle -->
             <div class="bg-white shadow-lg rounded-lg p-6 border-l-4 border-blue-500">
                 <h2 class="text-xl font-semibold text-gray-700 mb-4 flex items-center">
-                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"/>
-                    </svg>
+                    
                     Place Actuelle
                 </h2>
                 <div class="mt-2">
