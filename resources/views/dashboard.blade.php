ac5e1907860c022f7f2b4b667199c79ee1f81607
hedvi2005 <hedvi@nadenane.net>
2025-03-17 15:23:28 +0100

    Commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -125,9 +125,7 @@
             <!-- Actions -->
             <div class="bg-white shadow-lg rounded-lg p-6 border-l-4 border-purple-500">
                 <h2 class="text-xl font-semibold text-gray-700 mb-4 flex items-center">
-                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                    </svg>
+                  
                     Actions Disponibles
                 </h2>
                 <div class="mt-2">
