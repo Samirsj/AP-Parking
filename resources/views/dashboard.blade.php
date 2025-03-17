db32abe8768c8752c6e20e010c58c377c7e3475f
hedvi2005 <hedvi@nadenane.net>
2025-03-17 15:27:52 +0100

    supression de la police

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -100,9 +100,7 @@
             <!-- File d'attente -->
             <div class="bg-white shadow-lg rounded-lg p-6 border-l-4 border-yellow-500">
                 <h2 class="text-xl font-semibold text-gray-700 mb-4 flex items-center">
-                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"/>
-                    </svg>
+                    
                     File d'Attente
                 </h2>
                 <div class="mt-2">
